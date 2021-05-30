87df58c82274f61d67fd3da305551145da96f841
Tyler Cleveland <46286834+Tyler-C2@users.noreply.github.com>
2021-05-30 10:37:48 -0400

    Update Tower_of_Hanoi.py
    Added sound effects. Overall, changed how code is executed. Changed disk list to classes.  Added restart button to main game. Added play again button to end of game

--- Tower_of_Hanoi.py
+++ Tower_of_Hanoi.py
@@ -3,7 +3,19 @@ import sys
 
 #intialize pygame
 pygame.init()
+
+# set screen
+screen = pygame.display.set_mode((800, 480))
+
+# sond effects for the game
+piece_up = pygame.mixer.Sound("piece_up.wav")
+piece_down = pygame.mixer.Sound("piece_down.wav")
+success = pygame.mixer.Sound("success.wav")
+
+# Title and Icon for the game window.
 pygame.display.set_caption("Tower of Hanoi")
+window_icon = pygame.image.load("icon.png")
+pygame.display.set_icon(window_icon)
 
 # colors
 black = (0,0,0)
@@ -19,9 +31,10 @@ btn_grey = (55,71,82)
 grey = (165, 165, 165)
 gold = (197,131,73)
 
+# Class to describe each tower and the available positions on that tower
 class Towers:
     def __init__(self):
-        self.base_pole = []
+        self.base_of_pole = []
         self.hitbox = []
         self.drawn_pos = []
 
@@ -38,12 +51,13 @@ class Towers:
         for xpos in range(50, 590+1, 270):
             base = pygame.Rect(xpos, 400, 160 , 20)
             pole = pygame.Rect(xpos+75, 190, 10, 210)
-            self.base_pole.append((base,pole))
+            self.base_of_pole.append((base,pole))
 
             self.hitbox.append(pygame.Rect(xpos+15, 190, 130, 210))   
 
             self.drawn_pos.append([pygame.Rect(xpos, ypos, 160 , 20) for ypos in range(200,375+1,25)])
 
+# Class to describe all game disks 
 class Disks:
     def __init__(self):
         self.disk_dict = {}
@@ -59,83 +73,97 @@ class Disks:
 
             disk_dict.update({disk:temp})
             width -= 9
-            count -= 1 
+            count -= 1
 
-def disk_start_pos(tower_pos,disk_dict, positions):
+def disk_start_pos(tower_pos, disk_dict, positions):
     current_pos = 7
 
     for num in range(len(disk_dict),0,-1):
         positions[0][current_pos] = num
         disk_dict[num].center = tower_pos[0][current_pos].center
-        current_pos -= 1
+        current_pos -= 1 
 
-def click_validator(cursor):
-    hitbox = new_towers.hitbox
-    positions = new_towers.positions
-    dict = new_disks.disk_dict
-    valid = False
-    t_hb = 0
-    og_tower_pos = 0
-    moving_disk = None
+# Describes values that are needed to start the game.  
+# Count is updated with the number of disks set by the player.
+class Start:
+    def __init__(self):
+        self.running = True
+        self.count = 3
 
+# Class that describes the current moving disk values. 
+# This class is used for moves that occur after the first move. 
+class Current_Disk:
+    def __init__(self):
+        self.valid_place = False # Stores if the moving disk can be placed to moved location 
+        self.idx_of_hb = 0 # Index on the tower the disk is moving to.
+        self.new_tower_pos = 0 # The tower the disk is moved to.
+
+# Class that describes the first move of the game 
+# and is updated with values of the subsequent valid move.
+class Previous_Disk:
+    def __init__(self):
+        self.valid_move = False # Stores if the clicked disk is able to be moved
+        self.tower_hitbox = 0 # The tower the clicked disk came from.
+        self.og_tower_pos = 0 # Position on the tower the clicked disk was moved from.
+        self.disk_number = None # The number of the disk 
+
+# validates player click to determine if a disk is moveable.
+# If it is the previous_disk object is updated with disk information.
+# previous_disk is object to hold disk info, hitbox is tower hitbox, positions is tower positions,
+# disk_dict is the dictionary of all game disks, and cursor is user cursor.
+def click_validator(previous_disk, hitbox, positions, disk_dict, cursor):
     for hb in hitbox:
         if hb.collidepoint(cursor):
-            t_hb = hitbox.index(hb)
-            for disk in dict: 
-                if dict[disk].collidepoint(cursor):
+            previous_disk.tower_hitbox = hitbox.index(hb)
+            for disk in disk_dict: 
+                if disk_dict[disk].collidepoint(cursor):
                     for pos in range(8):
                         if pos > 0:
-                            if positions[t_hb][pos] == disk and positions[t_hb][pos-1] == None:
-                                valid = True
-                                og_tower_pos = pos
-                                moving_disk = disk
+                            if positions[previous_disk.tower_hitbox][pos] == disk and positions[previous_disk.tower_hitbox][pos-1] == None:
+                                previous_disk.valid_move = True
+                                previous_disk.og_tower_pos = pos
+                                previous_disk.disk_number = disk
                                 break
-                        elif positions[t_hb][pos] == disk:
-                            valid = True
-                            og_tower_pos = pos
-                            moving_disk = disk
+                        elif positions[previous_disk.tower_hitbox][pos] == disk:
+                            previous_disk.valid_move = True
+                            previous_disk.og_tower_pos = pos
+                            previous_disk.disk_number = disk
                             break
             break        
-    return [valid,t_hb,og_tower_pos,moving_disk]
 
-def placement_validator(og_tower_hitbox,disk):
+# Validates if the place on screen is a valid placement. 
+# If it is the current_disk object is updated with that placement information. 
+def placement_validator(current_disk, new_towers, new_disks, og_tower_hitbox, disk):
     hitbox = new_towers.hitbox
     positions = new_towers.positions
     dict = new_disks.disk_dict
-    valid = False
-    idx_of_hb = 0
-    new_tower_pos = 0
 
     for hb in hitbox:
         if hb.colliderect(dict[disk]) and hb != og_tower_hitbox:
-            idx_of_hb = hitbox.index(hb)
+            current_disk.idx_of_hb = hitbox.index(hb)
             for pos in range(7,-1,-1):
-                if pos == 0 and positions[idx_of_hb][pos] == None:
-                    if positions[idx_of_hb][pos+1] > disk:
-                        new_tower_pos = pos
-                        valid = True 
+                if pos == 0 and positions[current_disk.idx_of_hb][pos] == None:
+                    if positions[current_disk.idx_of_hb][pos+1] > disk:
+                        current_disk.new_tower_pos = pos
+                        current_disk.valid_place = True 
                 else:
-                    if positions[idx_of_hb][pos] == None and positions[idx_of_hb][pos-1] == None:
+                    if positions[current_disk.idx_of_hb][pos] == None and positions[current_disk.idx_of_hb][pos-1] == None:
                         if pos < 7:
-                            if positions[idx_of_hb][pos+1] > disk:
-                                new_tower_pos = pos
-                                valid = True
+                            if positions[current_disk.idx_of_hb][pos+1] > disk:
+                                current_disk.new_tower_pos = pos
+                                current_disk.valid_place = True
                                 break
                             else:
                                 break
                         else:
-                            new_tower_pos = pos
-                            valid = True
+                            current_disk.new_tower_pos = pos
+                            current_disk.valid_place = True
                             break
-    return [valid,idx_of_hb,new_tower_pos]
 
-def main_menu():
-    global running
-    count = 3
+def start_menu(new_start):
     launch_game = False
     launch_on_up = False
-    increase = False
-    decrease = False
+
     while not launch_game:  
         cursor = pygame.mouse.get_pos()
         screen.fill(light_blue)
@@ -143,25 +171,23 @@ def main_menu():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 launch_game = True
-                running = False
-                return count
+                new_start.running = False
         
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if 200+100 > cursor[0] > 200 and 210+100 > cursor[1] > 210:
-                    count -= 1
-                    if count < 1: 
-                        count = 1   
+                    new_start.count -= 1
+                    if new_start.count < 1: 
+                        new_start.count = 1   
                 elif 500+100 > cursor[0] > 500 and 210+100 > cursor[1] > 210:
-                    count += 1
-                    if count > 8:
-                        count = 8
+                    new_start.count += 1
+                    if new_start.count > 8:
+                        new_start.count = 8
                 elif 300+200 > cursor[0] > 300 and 380+50 > cursor[1] > 380:
                     launch_on_up = True
 
             elif event.type == pygame.MOUSEBUTTONUP:
                 if launch_on_up:    
                     launch_game = True
-                    return count
 
         if 200+100 > cursor[0] > 200 and 210+100 > cursor[1] > 210:
             pygame.draw.rect(screen, on_btn_blue, (200,210,100,100))
@@ -180,7 +206,7 @@ def main_menu():
         
         blit_text(screen, "Tower of Hanoi", (400,50), font_name='sans serif', size=90, color=gold)
         blit_text(screen, 'Click " + " to increase number of disks and " - " to decrease:', (400, 140), font_name='sans serif', size=30, color=black)
-        blit_text(screen, str(count), (400, 240), font_name='sans serif', size=70, color=blue)
+        blit_text(screen, str(new_start.count), (400, 240), font_name='sans serif', size=70, color=blue)
         blit_text(screen, 'Play', (400, 398), font_name='sans_serif', size=30, color=black)  
         pygame.draw.rect(screen, btn_grey, (515,253,70,15)) 
         pygame.draw.rect(screen, btn_grey, (543,225,15,70))
@@ -191,16 +217,37 @@ def main_menu():
 def game_over(num_of_disks,moves):
     screen.fill(light_blue)
     still_end = True
+    play_again = False
     min_moves = 2**num_of_disks-1 
     while still_end:
+        cursor = pygame.mouse.get_pos()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if 300+200 > cursor[0] > 300 and 380+50 > cursor[1] > 380:
+                    play_again = True
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                    if play_again:  
+                        pygame.mixer.stop()
+                        return True 
+                        
 
         blit_text(screen, 'You Won!', (400,60), font_name='sans serif', size=90, color=gold)
         blit_text(screen, f'Your Moves: {moves}', (415, 250), font_name='mono', size=30, color=black)
         blit_text(screen, f'Minimum Moves: {min_moves}', (410, 310), font_name='mono', size=30, color=red)
         
+        # play again button
+        if 300+200 > cursor[0] > 300 and 380+50 > cursor[1] > 380:
+            pygame.draw.rect(screen, on_btn_green, (300,380,200,50))
+        else:
+            pygame.draw.rect(screen, off_btn_green, (300,380,200,50))
+
+        blit_text(screen, 'Play Again?', (400, 398), font_name='sans_serif', size=30, color=black)
+
         if min_moves==moves:
             blit_text(screen, 'You finished in the least amount of moves!', (400, 160), font_name='mono', size=26, color=green)
         
@@ -213,71 +260,114 @@ def blit_text(screen, text, midtop, font_name, size, color):
     font_rect.midtop = midtop
     screen.blit(font_surface, font_rect)    
 
-# start of game    
-screen = pygame.display.set_mode((800, 480))
-new_towers = Towers()
-new_disks = Disks()
+# start of game
+def game_play():
+    moving = False
+    restart = False 
 
-running = True
-moving = False
+    # creates game objects.
+    new_towers = Towers()
+    new_disks = Disks()
+    new_start = Start()
+    current_disk = Current_Disk()
+    previous_disk = Previous_Disk()
 
-new_disks.disk_count = main_menu()
+    start_menu(new_start) 
 
-new_towers.make_towers(screen)
-new_disks.create_disks(new_disks.disk_count, new_disks.disk_dict)
-disk_start_pos(new_towers.drawn_pos,new_disks.disk_dict,new_towers.positions)
+    new_disks.disk_count = new_start.count 
 
-# game loop
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    new_towers.make_towers(screen)
+    new_disks.create_disks(new_disks.disk_count, new_disks.disk_dict)
+    disk_start_pos(new_towers.drawn_pos, new_disks.disk_dict, new_towers.positions)
 
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            cursor = pygame.mouse.get_pos()
-            previous_pos = click_validator(cursor)
-            if previous_pos[0]:
-                moving_rect = new_disks.disk_dict[previous_pos[-1]]
-                new_towers.positions[previous_pos[1]][previous_pos[2]] = None
-                if moving_rect.collidepoint(event.pos):
-                    moving = True
+    # game_loop
+    while new_start.running:
+        cursor = pygame.mouse.get_pos()
 
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if previous_pos[0]:
-                valid_place = placement_validator(previous_pos[2],previous_pos[3])
-                if valid_place[0]: 
-                    moving_rect.center = new_towers.drawn_pos[valid_place[1]][valid_place[-1]].center
-                    new_towers.positions[valid_place[1]][valid_place[2]] = previous_pos[-1]
-                    if previous_pos[1] != valid_place[1]:
-                        new_disks.total_moves+=1
-                else:
-                    moving_rect.center = new_towers.drawn_pos[previous_pos[1]][previous_pos[2]].center
-                    new_towers.positions[previous_pos[1]][previous_pos[2]] = previous_pos[-1]
-                moving_rect = None
-                previous_pos[0] = False        
-                moving = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                new_start.running = False
 
-        elif event.type == pygame.MOUSEMOTION and moving:
-            moving_rect.move_ip(event.rel)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                cursor = pygame.mouse.get_pos()
+                click_validator(previous_disk, new_towers.hitbox, new_towers.positions, new_disks.disk_dict, cursor)
+                if previous_disk.valid_move:
+                    moving_rect = new_disks.disk_dict[previous_disk.disk_number]
+                    new_towers.positions[previous_disk.tower_hitbox][previous_disk.og_tower_pos] = None
+                    if moving_rect.collidepoint(event.pos):
+                        pygame.mixer.Sound.play(piece_up)
+                        moving = True
+                # restart check          
+                if 10+150 > cursor[0] > 10 and 440+30 > cursor[1] > 440:
+                    restart = True
 
-    screen.fill(light_blue)
-    
-    blit_text(screen, "To win: move all disk to the right most tower, in ascending order.", (400,20), font_name='sans serif', size=30, color=black)
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if previous_disk.valid_move:
+                    placement_validator(current_disk, new_towers, new_disks, previous_disk.og_tower_pos, previous_disk.disk_number)
+                    if current_disk.valid_place: 
+                        moving_rect.center = new_towers.drawn_pos[current_disk.idx_of_hb][current_disk.new_tower_pos].center
+                        new_towers.positions[current_disk.idx_of_hb][current_disk.new_tower_pos] = previous_disk.disk_number
+                        if previous_disk.tower_hitbox != current_disk.idx_of_hb:
+                            new_disks.total_moves+=1
+                    else:
+                        moving_rect.center = new_towers.drawn_pos[previous_disk.tower_hitbox][previous_disk.og_tower_pos].center
+                        new_towers.positions[previous_disk.tower_hitbox][previous_disk.og_tower_pos] = previous_disk.disk_number
+                    moving_rect = None
+                    previous_disk.valid_move = False
+                    current_disk.valid_place = False
+                    pygame.mixer.Sound.play(piece_down)       
+                    moving = False
+                # restart check 
+                if restart:    
+                    return [100, 0]
 
-    # draws towers
-    for tower in new_towers.base_pole:
-        pygame.draw.rect(screen, green, tower[0])
-        pygame.draw.rect(screen, grey, tower[1])
+            elif event.type == pygame.MOUSEMOTION and moving:
+                moving_rect.move_ip(event.rel)
 
-    # draws disks
-    for disk in range(new_disks.disk_count,0,-1):
-        pygame.draw.rect(screen, blue, new_disks.disk_dict[disk])
-
-    blit_text(screen, f"Moves: {new_disks.total_moves}", (700, 65), font_name='mono', size=30, color=black)
-
-    # end of game check
-    if new_towers.positions[2][8-new_disks.disk_count] != None:
-        running = game_over(new_disks.disk_count,new_disks.total_moves)
-
-    pygame.display.update()    
+        screen.fill(light_blue)
         
+        blit_text(screen, "To win: move all disk to the right most tower, in ascending order.", (400,20), font_name='sans serif', size=30, color=black)
+
+        # draws towers
+        for tower in new_towers.base_of_pole:
+            pygame.draw.rect(screen, green, tower[0])
+            pygame.draw.rect(screen, grey, tower[1])
+
+        # draws disks
+        for disk in range(new_disks.disk_count,0,-1):
+            pygame.draw.rect(screen, blue, new_disks.disk_dict[disk])
+
+        # restart game button
+        if 10+100 > cursor[0] > 10 and 440+30 > cursor[1] > 440:
+            pygame.draw.rect(screen, on_btn_green, (10,440,100,30))
+        else:
+            pygame.draw.rect(screen, off_btn_green, (10,440,100,30))
+
+        blit_text(screen, 'Restart', (60, 447), font_name='sans_serif', size=30, color=black)
+
+        blit_text(screen, f"Moves: {new_disks.total_moves}", (700, 65), font_name='mono', size=30, color=black)
+
+        # end of game check returns list containing number of disks and total player moves to main.
+        if new_towers.positions[2][8-new_disks.disk_count] != None:
+            pygame.mixer.Sound.play(success)
+            new_start.running = False
+            return [new_disks.disk_count, new_disks.total_moves]
+            #new_start.running = game_over(new_disks.disk_count,new_disks.total_moves)
+
+        pygame.display.update()   
+
+# main calls game loop and game over and checks if the player wants to continue playing.
+def main(): 
+    play = True
+    while play:
+        game_results = game_play()
+        try:
+            if game_results[0] == 100: # used when restart button is pressed during game
+                pass
+            else:    
+                play = game_over(game_results[0],game_results[1])
+        except:
+            play = False
+
+main()
+pygame.quit() # quits game for good     
